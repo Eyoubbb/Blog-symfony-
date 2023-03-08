4af9e8ff918b196d5890dfe1508c7298df3ab1c8
Jérémy Boureux <jeremyboureux@gmail.com>
2023-03-08 17:42:06 +0100

    implémentation contrats des controlleurs

--- src/Controller/admin/AdminCategoryController.php
+++ src/Controller/admin/AdminCategoryController.php
@@ -2,7 +2,48 @@
 
 namespace App\Controller\admin;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class AdminCategoryController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
 {
+    /**
+     * Controlleur pour afficher la liste des catégories. 20 elements par page
+     * @return Response
+     * @author Jérémy
+     */
+    #[Route('/admin/categories', methods:['get'])]
+    public function categories(): Response{
+        return $this->render('base.html.twig');
+    }
 
+    /**
+     * Controlleur pour créer une catégorie.
+     * @return Response
+     * @author Jérémy
+     */
+    #[Route('/admin/categories/create', methods:['get'])]
+    public function categoriesCreate(): Response{
+        return $this->render('base.html.twig');
+    }
+
+    /**
+     * Controlleur pour modifier une catégorie.
+     * @return Response
+     * @author Jérémy
+     */
+    #[Route('/admin/categories/{id}/edit', methods:['get'])]
+    public function categoriesEdit(): Response{
+        return $this->render('base.html.twig');
+    }
+
+    /**
+     * Controlleur pour supprimer une catégorie.
+     * @return Response
+     * @author Jérémy
+     */
+    #[Route('/admin/categories/{id}/delete', methods:['get'])]
+    public function categoriesDelete(): Response{
+        return $this->render('base.html.twig');
+    }
 }
--- src/Controller/user/CommentsController.php
+++ src/Controller/user/CommentsController.php
@@ -2,7 +2,18 @@
 
 namespace App\Controller\user;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class CommentsController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
 {
-
+    /**
+     * Controlleur pour modifier une catégorie.
+     * @return Response
+     * @author Jérémy
+     */
+    #[Route('/posts/{id}/comment', methods:['get'])]
+    public function comment(): Response{
+        return $this->render('base.html.twig');
+    }
 }
